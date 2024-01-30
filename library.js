63bb4a39f651b96f9eb93634903970cbf5a7ee2d
Jessye Lam <jessyel13@gmail.com>
2024-01-29 22:42:13 -0500

    add book functionality created

--- library.js
+++ library.js
@@ -1,18 +1,45 @@
 const myLibrary = [];
 
-function Book(name, read) {
-  // the constructor...
-  this.name = name;
-  this.read = read;
+function Book(title, author, pages, read) {
+    // the constructor...
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary(book) {
-    myLibrary.push(book); 
+    myLibrary.push(book);
+    displayBooks()
 }
 
 function displayBooks() {
     const booksDisplay = document.querySelector('#booksDisplay')
-    for (book in myLibrary) {
-        booksDisplay.innerHTML += "<tr><td>" + book.name + "</td></tr>"
-    }
+    booksDisplay.innerHTML = ""
+    booksDisplay.innerHTML += "<tr><th>Book Title</th><th>Book Author</th><th>Book Pages</th><th>Book Read</th></tr>"
+    myLibrary.forEach(function (book) {
+        booksDisplay.innerHTML += "<tr><td>" + book.title + "</td><td>" + book.author + "</td><td>" + book.pages + "</td><td>" + book.read + "</td</tr>"
+    });
 }
+
+const bookForm = document.getElementById("bookForm");
+
+bookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // handle submit
+    let title = document.getElementById("book-title")
+    let author = document.getElementById("book-author")
+    let pages = document.getElementById("book-pages")
+    let read = document.getElementById("book-read")
+
+    let book = new Book(title.value, author.value, pages.value, read.value)
+
+    if(title.value == '' || author.value == '' || pages.value == '' || read.value == ''){
+        document.getElementById("error").innerHTML += 'Please fill out all the fields<br>'
+    }
+    else{
+        document.getElementById("error").innerHTML = ''
+    addBookToLibrary(book)
+    }
+});
